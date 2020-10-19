16153ade93909ffbc1636d780a6f60c09f2c5f3c
archan <joshua_0621@naver.com>
2020-10-19 17:43:59 +0900

    2020 10 19
    final

--- JS_02.js
+++ JS_02.js
@@ -6,16 +6,40 @@
 
 // console.log(window.b);
 
-var sample = {
-    name : "abc"
+// var sample = {
+//     name : "abc"
+// }
+
+// const server_info = {
+//     ip:'12.1.1.1',
+//     port : 80
+// }
+
+// var name = "xyz"
+
+// function print(){
+//     console.log(this.ip)
+// }
+
+//print();
+
+// print.call(server_info);
+
+// bind vs call /apply
+
+var tmp= {
+    name:"abc",
+    func:function () {
+        alert(this.name)
+    }
 }
 
-var name = "xyz"
-
-function print(){
-    console.log(this.name)
+var tmp2 = {
+    name: "xyz",
 }
 
-print();
+var func2 = tmp.func.bind(tmp2);
 
-print.call();
+func2();
+
+tmp.func();
